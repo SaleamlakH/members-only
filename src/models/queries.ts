21f9209f7113593860b2cfa6785067ffadb3a10b
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-04 17:16:42 +0300

    feat(db): add deleteUsers function

--- src/models/queries.ts
+++ src/models/queries.ts
@@ -1,7 +1,6 @@
 import pool from './pool';
 
-// account creation inserts username, email, and password
-// to users table
+// account creation inserts username, email, and password to users table
 const createUser = async (username: string, email: string, password: string) => {
   try {
     await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [
@@ -14,4 +13,13 @@ const createUser = async (username: string, email: string, password: string) => 
   }
 };
 
-export { createUser };
+// delete account remove a user from users table
+const deleteUser = async (userId: number) => {
+  try {
+    await pool.query('DELETE FROM users WHERE id = $1', [userId]);
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { createUser, deleteUser };

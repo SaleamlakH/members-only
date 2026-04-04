be57128dc1e506390e1ae809f01cdacb9045d2a1
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-04 17:45:53 +0300

    feat(users): add updateProfileInfo and updatePassword functions
    These are database functions interacting with the users table to update
    profile info and passwords.

--- src/models/queries/users.ts
+++ src/models/queries/users.ts
@@ -14,4 +14,17 @@ const deleteUser = async (userId: number) => {
   return pool.query('DELETE FROM users WHERE id = $1', [userId]);
 };
 
-export { createUser, deleteUser };
+// update user
+const updateProfileInfo = async (userId: number, username: string, email: string) => {
+  return pool.query('UPDATE users SET username = $1, email = $2 WHERE id = $3', [
+    username,
+    email,
+    userId,
+  ]);
+};
+
+const updatePassword = async (userId: number, password: string) => {
+  return pool.query('UPDATE users SET password = $1 WHERE id = $2', [password, userId]);
+};
+
+export { createUser, deleteUser, updateProfileInfo, updatePassword };

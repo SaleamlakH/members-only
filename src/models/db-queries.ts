c6510d824e714ae6144d8e81440cf05da03c655f
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-05 14:56:34 +0300

    feat(db): add centralized exports for query modules

---
+++ src/models/db-queries.ts
@@ -0,0 +1,8 @@
+import * as users from '@/models/queries/users';
+import * as groups from '@/models/queries/groups';
+import * as messages from '@/models/queries/messages';
+import * as groupMessages from '@/models/queries/group-messages';
+import * as groupMembers from '@/models/queries/group-members';
+import * as groupAdmins from '@/models/queries/group-admins';
+
+export { users, groups, messages, groupMembers, groupMessages, groupAdmins };

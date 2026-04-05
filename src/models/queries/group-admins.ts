5421f6a9106436a3ecb143f63c52b7da90265208
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-05 10:48:33 +0300

    feat(group-admins): add addAdmin, deleteAdmin, and isAdmin functions

---
+++ src/models/queries/group-admins.ts
@@ -0,0 +1,27 @@
+import pool from '../pool';
+import type { UserGroupRelation } from '@/types/db';
+
+const table = 'group_admins';
+
+const addAdmin = async ({ userId, groupId }: UserGroupRelation) => {
+  return pool.query(`INSERT INTO ${table} (user_id, group_id) VALUES ($1, $2);`, [userId, groupId]);
+};
+
+const deleteAdmin = async ({ userId, groupId }: UserGroupRelation) => {
+  return pool.query(`DELETE FROM ${table} WHERE user_id = $1 AND group_id = $2;`, [
+    userId,
+    groupId,
+  ]);
+};
+
+// check if a user is a member
+const isAdmin = async ({ userId, groupId }: UserGroupRelation) => {
+  const { rowCount } = await pool.query(
+    `SELECT 1 FROM ${table} WHERE user_id = $1 AND group_id = $2 LIMIT 1;`,
+    [userId, groupId],
+  );
+
+  return rowCount;
+};
+
+export { addAdmin, deleteAdmin, isAdmin };

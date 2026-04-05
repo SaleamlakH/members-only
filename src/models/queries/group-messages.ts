22387cff4ef4bf9c55777b79eac78d9c6eb9ff0f
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-05 10:15:49 +0300

    feat(group-messages): add function to fetch messages for a group

---
+++ src/models/queries/group-messages.ts
@@ -0,0 +1,14 @@
+import pool from '../pool';
+import type { MessageGroupRelation } from '@/types/db';
+
+// filter all messages of a group
+const getGroupMessages = async ({ groupId }: MessageGroupRelation) => {
+  const { rows } = await pool.query(
+    'SELECT * FROM messages WHERE id IN (SELECT message_id FROM group_messages WHERE group_id = $1);',
+    [groupId],
+  );
+
+  return rows;
+};
+
+export { getGroupMessages };

75efaaa2b5c6d24695e859cce5d9e8b55084ac25
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-05 09:34:07 +0300

    refactor: add semicolon at the end of queries in users and groups

--- src/models/queries/users.ts
+++ src/models/queries/users.ts
@@ -3,7 +3,7 @@ import DB from '@/types/db';
 
 // account creation inserts username, email, and password to users table
 const createUser = async ({ username, email, password }: DB.User) => {
-  return pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3)', [
+  return pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3);', [
     username,
     email,
     password,
@@ -11,18 +11,18 @@ const createUser = async ({ username, email, password }: DB.User) => {
 };
 
 const getUser = async ({ id }: DB.User) => {
-  const { rows } = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+  const { rows } = await pool.query('SELECT * FROM users WHERE id = $1;', [id]);
   return rows[0];
 };
 
 // delete account remove a user from users table
 const deleteUser = async ({ id }: DB.User) => {
-  return pool.query('DELETE FROM users WHERE id = $1', [id]);
+  return pool.query('DELETE FROM users WHERE id = $1;', [id]);
 };
 
 // update user
 const updateProfileInfo = async ({ id, username, email }: DB.User) => {
-  return pool.query('UPDATE users SET username = $1, email = $2 WHERE id = $3', [
+  return pool.query('UPDATE users SET username = $1, email = $2 WHERE id = $3;', [
     username,
     email,
     id,
@@ -30,7 +30,7 @@ const updateProfileInfo = async ({ id, username, email }: DB.User) => {
 };
 
 const updatePassword = async ({ id, password }: DB.User) => {
-  return pool.query('UPDATE users SET password = $1 WHERE id = $2', [password, id]);
+  return pool.query('UPDATE users SET password = $1 WHERE id = $2;', [password, id]);
 };
 
 export { createUser, getUser, deleteUser, updateProfileInfo, updatePassword };

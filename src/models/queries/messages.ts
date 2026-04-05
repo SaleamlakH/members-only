97f093a4082ed0919c4abd73205d1860a8a82b59
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-05 09:32:36 +0300

    feat(messages): add CRUD function for messages table

---
+++ src/models/queries/messages.ts
@@ -0,0 +1,31 @@
+import pool from '../pool';
+import type { Messages } from '@/types/db';
+
+const table = 'messages';
+
+const createMessage = async ({ title, content, authorId }: Messages) => {
+  return pool.query(`INSERT INTO ${table} (title, content, author_id) VALUES ($1, $2, $2);`, [
+    title,
+    content,
+    authorId,
+  ]);
+};
+
+const getMessage = async ({ id }: Messages) => {
+  const { rows } = await pool.query(`SELECT * FROM ${table} WHERE id = $1;`, [id]);
+  return rows[0];
+};
+
+const updateMessage = async ({ id, title, content }: Messages) => {
+  return pool.query(`UPDATE ${table} SET title = $1, content = $2 WHERE id = $3;`, [
+    title,
+    content,
+    id,
+  ]);
+};
+
+const deleteMessage = async ({ id }: Messages) => {
+  return pool.query(`DELETE FROM ${table} WHERE id = $1;`, [id]);
+};
+
+export { createMessage, getMessage, updateMessage, deleteMessage };

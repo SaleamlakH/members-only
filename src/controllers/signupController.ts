5f4f8a64143a248c3952ff4867b57b47d0324210
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-06 18:55:05 +0300

    feat(signup): implement signup controller with validator

---
+++ src/controllers/signupController.ts
@@ -0,0 +1,68 @@
+import type { NextFunction, Request, Response } from 'express';
+import { body, validationResult, matchedData, type CustomValidator } from 'express-validator';
+import bcrypt from 'bcryptjs';
+import * as db from '@/models/db-queries';
+import type { UserCreate } from '@/types/db';
+
+const isUsernameTaken: CustomValidator = async (username) => {
+  const user = await db.users.getUserByUsername(username);
+  if (user) {
+    throw new Error('Username already taken');
+  }
+};
+
+const isEmailExist: CustomValidator = async (email) => {
+  const user = await db.users.getUserByEmail(email);
+  if (user) {
+    throw new Error('Email already exist');
+  }
+};
+
+// validate signup form
+const validator = [
+  body('username')
+    .trim()
+    .escape()
+    .isLength({ min: 3 })
+    .withMessage('Username must be at least 3 characters')
+    .custom(isUsernameTaken),
+
+  body('email').isEmail().withMessage('Invalid email address').custom(isEmailExist),
+
+  body('password')
+    .trim()
+    .isLength({ min: 8 })
+    .withMessage('Password must be at least 8 characters'),
+];
+
+const userCreatePost = [
+  validator,
+  async (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.locals['errors'] = errors.array();
+      return next();
+    }
+
+    // hash password
+    const { username, email, password } = matchedData(req) as UserCreate;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // save it to database
+    const user = await db.users.createUser({
+      username: username.toLowerCase(),
+      email: email.toLowerCase(),
+      password: hashedPassword,
+    });
+
+    // log in and redirect
+    req.login(user, (err) => {
+      if (err) throw err;
+
+      // must be replaced with render after we have the view
+      res.status(200).json(user);
+    });
+  },
+];
+
+export default userCreatePost;

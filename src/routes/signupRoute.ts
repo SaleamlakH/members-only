494cffde196534e54a2b5c28f920507e3fecda83
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-06 20:52:24 +0300

    feat(signup): add middleware which passes validation errors to form view

--- src/routes/signupRoute.ts
+++ src/routes/signupRoute.ts
@@ -1,8 +1,23 @@
 import userCreatePost from '@/controllers/signupController';
-import { Router } from 'express';
+import type { FormValidationError } from '@/types/validation';
+import { Router, type Request, type Response } from 'express';
+import type { FieldValidationError } from 'express-validator';
 
 const signupRouter = Router();
 
-signupRouter.post('/', ...userCreatePost);
+signupRouter.post('/', ...userCreatePost, (req: Request, res: Response) => {
+  // map errors
+  const validationErrors = (res.locals['errors'] as FieldValidationError[]).reduce(
+    (errors, { value, msg, path }: FieldValidationError) => {
+      errors[path] = { value, message: msg };
+      return errors;
+    },
+    {} as FormValidationError,
+  );
+
+  // comment out and remove json response when the view is ready
+  res.status(400).json({ data: req.body, errors: validationErrors });
+  // res.status(400).render('signup-form', { data: req.body, errors: validationErrors });
+});
 
 export default signupRouter;

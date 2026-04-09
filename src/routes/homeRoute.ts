6393bf013119b75a7509043a28be4166c699ac86
SaleamlakH <saleamlak.7h@gmail.com>
2026-04-09 15:19:25 +0300

    feat(navbar): check the path to render login and signup nav links

--- src/routes/homeRoute.ts
+++ src/routes/homeRoute.ts
@@ -3,7 +3,7 @@ import { Router, type NextFunction, type Request, type Response } from 'express'
 const homeRouter = Router();
 
 homeRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.render('pages/home', { title: 'Members Only', user: req.user });
+  res.render('pages/home', { title: 'Members Only', user: req.user, path: '/' });
 });
 
 export default homeRouter;
